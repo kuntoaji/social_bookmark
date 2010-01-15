1ec24863ba232f53d52c087b7734fd49f851de82
Kunto Aji Kristianto <aji_okay@yahoo.co.id>
2010-01-15 15:16:12 +0700

    fixin bug copying images to public/images/social_bookmark

--- install.rb
+++ install.rb
@@ -8,12 +8,13 @@ FileUtils.cp(config, dest) unless File.exists? dest
 
 puts "** Copying all images to public/images/social_bookmark/ ..."
 images = File.join(RAILS_ROOT, '/vendor/plugins/social_bookmark/images/*')
-dest = File.join(RAILS_ROOT, '/public/images/social_bookmark/*')
+dest = File.join(RAILS_ROOT, '/public/images/social_bookmark/')
 
 unless File.exists? dest
   FileUtils.cd images
   FileUtils.mkdir dest
-  FileUtils.cp_r Dir.glob('*.png'), dest, :noop => true, :verbose => true  
+  FileUtils.cp(images, dest) unless File.exists? dest
+#  FileUtils.cp_r Dir.glob('*.png'), dest, :noop => true, :verbose => true  
 end
 
 puts "** Installation finished, edit your configuration file to select your bookmark list and restart your application afterwards ...." 
