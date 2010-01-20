0e2f22922d1606984c54c9e107d98bcf0baace3a
Kunto Aji Kristianto <aji_okay@yahoo.co.id>
2010-01-20 23:30:43 +0700

    updating README

--- install.rb
+++ install.rb
@@ -4,7 +4,7 @@ puts "** Installing Social Bookmarks Plugin...."
 config = File.join(RAILS_ROOT, '/vendor/plugins/social_bookmark/lib/sites_EN.xml')
 dest = File.join(RAILS_ROOT, '/config/sites_EN.xml')
 puts "** Copying config file into #{dest} ...." 
-FileUtils.cp(config, dest) unless File.exists? dest
+FileUtils.mv(config, dest) unless File.exists? dest
 
 images_parent = File.join(RAILS_ROOT, '/vendor/plugins/social_bookmark/')
 dest = File.join(RAILS_ROOT, '/public/images/social_bookmark/')
