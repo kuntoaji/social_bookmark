98a8c80495e3a106fc46011da33d35bdd1c50fb7
Kunto Aji Kristianto <aji_okay@yahoo.co.id>
2010-01-20 22:59:03 +0700

    create uninstall.rb

--- uninstall.rb
+++ uninstall.rb
@@ -1,1 +1,10 @@
 # Uninstall hook code here
+# Removing sites_EN.xml
+puts "Removing /config/sites_EN.xml"
+dest = File.join(RAILS_ROOT, '/config/sites_EN.xml')
+FileUtils.rm(dest) if File.exists?(dest)
+
+# Removing directory public/images/social_bookmark/
+puts "Removing /public/images/social_bookmark/"
+dest = File.join(RAILS_ROOT, '/public/images/social_bookmark/')
+FileUtils.rm_rf(dest) if File.exists?(dest)
