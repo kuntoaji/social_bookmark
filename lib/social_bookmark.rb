4ce5f9494b6fdec9e4b30e1bd8a645a5d30a3164
Kunto Aji Kristianto <aji_okay@yahoo.co.id>
2010-01-20 21:13:30 +0700

    fixin bug when using parse_config_with_options

--- lib/social_bookmark.rb
+++ lib/social_bookmark.rb
@@ -6,7 +6,7 @@ module SocialBookmark
     def to_s; title end
   end
 
-  def parse_config(permalink, title)
+  def parse_config(permalink, title, options)
     xml = REXML::Document.new(File.open("#{RAILS_ROOT}/config/sites_EN.xml"))
     items = []
 
@@ -16,10 +16,8 @@ module SocialBookmark
       item.url    = REXML::XPath.match(elem, "url/text()").first.value.gsub('{link}', 
                     permalink).gsub('{title}', CGI::escape(title)) rescue ""
       item.image  = REXML::XPath.match(elem, "img/text()").first.value rescue ""
-      
       items << item
     end
-    
     items.sort_by { |item| item.name }
   end
 
@@ -28,43 +26,42 @@ module SocialBookmark
     items = []
 
     # NOT DRY, ouch..
-    unless options.empty?
-      options.each do |name|
-        REXML::XPath.each(xml, "social_sites/site/") do |elem|
-          item_name   = REXML::XPath.match(elem, "name/text()").first.value rescue ""
-	  if name.downcase == item_name.downcase
-            item        = SocialItem.new
-            item.name   = REXML::XPath.match(elem, "name/text()").first.value rescue ""
-	    item.name   = item_name
-            item.url    = REXML::XPath.match(elem, "url/text()").first.value.gsub('{link}', 
-                        permalink).gsub('{title}', CGI::escape(title)) rescue ""
-            item.image  = REXML::XPath.match(elem, "img/text()").first.value rescue ""
-          
-            items << item
-	  end
+    options.each do |name|
+      REXML::XPath.each(xml, "social_sites/site/") do |elem|
+        item_name   = REXML::XPath.match(elem, "name/text()").first.value rescue ""
+        if name.downcase == item_name.downcase
+          item        = SocialItem.new
+          item.name   = item_name
+          item.url    = REXML::XPath.match(elem, "url/text()").first.value.gsub('{link}', 
+			permalink).gsub('{title}', CGI::escape(title)) rescue ""
+          item.image  = REXML::XPath.match(elem, "img/text()").first.value rescue ""
+          items << item
         end
       end
     end
-    
     items.sort_by { |item| item.name }
   end
 end
 
 module SocialBookmarkHelper
   def render_social_bookmarks(permalink, title, options = [])
-  options.empty? ? items = @controller.parse_config(permalink, title) : @controller.parse_config_with_options(permalink, title, options)
-    
-    output = "<p id='social'>"
-    
-    items.each do |item|
-      output << "<span>"
-      output << "<a href='#{item.url}'>"
-      output << "<img src='/images/social_bookmark/#{item.image}' alt='Add to #{item.name}' /> "
-      output << "#{item.name}"
-      output << "</a>"
-      output << "</span> "
-    end
+    # options = array of bookmarks sites
+    # example ['facebook', 'furl']
+    options.empty? ? items = @controller.parse_config(permalink, title) : items = @controller.parse_config_with_options(permalink, title, options)
 
-    output << "</p>"
+    unless items.nil? || items.empty?
+      output = "<p id='social'>"
+      items.each do |item|
+        output << "<span>"
+        output << "<a href='#{item.url}'>"
+        output << "<img src='/images/social_bookmark/#{item.image}' alt='Add to #{item.name}' /> "
+        output << "#{item.name}"
+        output << "</a>"
+        output << "</span> "
+      end
+
+      output << "</p>"
+    end
   end
 end
+
