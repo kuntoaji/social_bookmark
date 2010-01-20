0cfe03d53f0a03207e727bfaaa960c32f2c6f59f
Kunto Aji Kristianto <aji_okay@yahoo.co.id>
2010-01-20 21:37:30 +0700

    fixing bug at install.rb..again? oh no

--- install.rb
+++ install.rb
@@ -9,7 +9,12 @@ FileUtils.cp(config, dest) unless File.exists? dest
 images_parent = File.join(RAILS_ROOT, '/vendor/plugins/social_bookmark/')
 dest = File.join(RAILS_ROOT, '/public/images/social_bookmark/')
 
-FileUtils.exists?(dest) ? FileUtils.rm_rf(dest) : FileUtils.mkdir(dest)
+# why not using FileUtils.exists?(dest) : FileUtils.rm_rf(dest) : FileUtils.rm_rf(dest)
+if FileUtils.exists? dest
+  FileUtils.rm_rf dest
+else
+  FileUtils.mkdir dest
+end
 puts "** Moving all images to public/images/social_bookmark/ ..."
 FileUtils.cd images_parent
 FileUtils.mv 'images/', dest
