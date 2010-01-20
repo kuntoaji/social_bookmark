f971ac379cdf8916638ce9b1bc23b5efd76bdd9f
Kunto Aji Kristianto <aji_okay@yahoo.co.id>
2010-01-20 21:23:41 +0700

    adding twitter

--- install.rb
+++ install.rb
@@ -9,10 +9,8 @@ FileUtils.cp(config, dest) unless File.exists? dest
 images_parent = File.join(RAILS_ROOT, '/vendor/plugins/social_bookmark/')
 dest = File.join(RAILS_ROOT, '/public/images/social_bookmark/')
 
-unless File.exists? dest
-  puts "** Moving all images to public/images/social_bookmark/ ..."
-  FileUtils.cd images_parent
-  FileUtils.mv 'images/', dest
-end
+puts "** Moving all images to public/images/social_bookmark/ ..."
+FileUtils.cd images_parent
+FileUtils.mv 'images/', dest
 
 puts "** Installation finished, edit your configuration file to select your bookmark list and restart your application afterwards ...." 
