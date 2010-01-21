23e1b2a8c613ccb86d4879f43a3afcddc148b240
Kunto Aji Kristianto <aji_okay@yahoo.co.id>
2010-01-21 07:47:57 +0700

    fixin install.rb when moving images

--- install.rb
+++ install.rb
@@ -3,15 +3,16 @@ puts "** Installing Social Bookmarks Plugin...."
 
 config = File.join(RAILS_ROOT, '/vendor/plugins/social_bookmark/lib/sites_EN.xml')
 dest = File.join(RAILS_ROOT, '/config/sites_EN.xml')
-puts "** Copying config file into #{dest} ...." 
+puts "** Moving config file into #{dest} ...." 
 FileUtils.mv(config, dest) unless File.exists? dest
 
 images_parent = File.join(RAILS_ROOT, '/vendor/plugins/social_bookmark/')
-dest = File.join(RAILS_ROOT, '/public/images/social_bookmark/')
+dest = File.join(RAILS_ROOT, '/public/images/')
 
 File.exists?(dest) ? FileUtils.rm_rf(dest) : FileUtils.mkdir(dest)
 puts "** Moving all images to public/images/social_bookmark/ ..."
 FileUtils.cd images_parent
-FileUtils.mv 'images/', dest
+File.rename 'images', 'social_bookmark'
+FileUtils.mv 'social_bookmark/', dest
 
 puts "** Installation finished, edit your configuration file to select your bookmark list and restart your application afterwards ...." 
